fad2b8497d3925c888a2f81c3bc443647553341a
C.Dean.C <constantine.cazolis@mds.torrens.edu.au>
2021-08-19 14:21:59 +1000

    level update

---
+++ Assets/UI Stuff/CountdownTimer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CountdownTimer : MonoBehaviour
+{
+    public string LevelToLoad;
+    private float timer = 50f;
+    private Text timerSeconds;
+    
+    void Start()
+    {
+        timerSeconds = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        timerSeconds.text = timer.ToString("f2");
+        if (timer <= 0)
+        {
+            Application.LoadLevel(LevelToLoad);
+        }
+    }
+}
